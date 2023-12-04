a501ecd88c3a6f3931e076e6c1d396ab4964d7f8
karinadityas <karina.adityasramadhanti2041@students.unila.ac.id>
2023-12-04 13:53:07 +0700

    edit tipiz

--- app/Views/toko/supply.php
+++ app/Views/toko/supply.php
@@ -1,7 +1,8 @@
 <?= $this->extend('templates/template') ?>
 <?= $this->section('content') ?>
 
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
+    navbar-scroll="true">
     <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
@@ -14,15 +15,14 @@
 </nav>
 
 <div class="container-fluid py-4">
-    <div>
-        <!-- Pemasukan -->
-        <div>
+    <div class="row">
+        <!-- Barang Masuk -->
+        <div class="col-6">
             <div class="card mb-4">
                 <div class="card-header pb-0">
-
                     <div class="row">
                         <div class="col-6 d-flex align-items-center">
-                            <h6 class="mb-0">Pemasukan</h6>
+                            <h6 class="mb-0">Barang Masuk</h6>
                         </div>
                         <div class="col-6 text-end">
                             <a href="/tambahSupply" type="button" class="btn btn-outline-primary btn-sm mb-0">Tambah</a>
@@ -36,15 +36,20 @@
                                 <tr>
                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No
                                     </th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                    <th
+                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                                         Jenis</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                                         Jumlah</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                                         Nominal</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                                         Tanggal</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                                         Aksi</th>
                                 </tr>
                             </thead>
@@ -52,35 +57,118 @@
                                 <?php $no = 1;
                                 foreach ($supply as $supply) :
                                 ?>
-                                    <tr>
-                                        <td class="align-middle text-center">
-                                            <span class="text-secondary text-xs font-weight-bold"><?= $no++ ?></span>
-                                        </td>
-                                        <td>
-                                            <p class="text-xs font-weight-bold mb-0"><?= $supply['nama_supply'] ?></p>
-                                        </td>
-                                        <td class="align-middle text-center text-sm">
-                                            <p class="text-xs font-weight-bold mb-0"><?= $supply['jumlah_supply'] ?></p>
-                                        </td>
-                                        <td class="align-middle text-center">
-                                            <span class="text-secondary text-xs font-weight-bold">Rp <?= number_format($supply['harga_supply']) ?></span>
-                                        </td>
-                                        <td class="align-middle text-center">
-                                            <span class="text-secondary text-xs font-weight-bold"><?= $supply['tanggal_supply'] ?></span>
-                                        </td>
-                                        <td>
-                                            <div class="d-flex" align="center">
-                                                <form method="POST" action="/hapusSupply/<?= $supply['id_supply'] ?>">
-                                                    <input type="hidden" name="_method" value="DELETE">
-                                                    <button type="submit" name="submit" data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus" class="btn btn-link text-danger text-gradient px-1 mb-0"><i class="far fa-trash-alt me-2"></i></button>
-                                                </form>
-                                                <a href="/editSupply/<?= $supply['id_supply'] ?>" type="button" class="btn btn-link text-dark px-1 mb-0"><i class="fa fa-pencil"></i></a>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-bold"><?= $no++ ?></span>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0"><?= $supply['nama_supply'] ?></p>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <p class="text-xs font-weight-bold mb-0"><?= $supply['jumlah_supply'] ?></p>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-bold">Rp
+                                            <?= number_format($supply['harga_supply']) ?></span>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span
+                                            class="text-secondary text-xs font-weight-bold"><?= $supply['tanggal_supply'] ?></span>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex" align="center">
+                                            <form method="POST" action="/hapusSupply/<?= $supply['id_supply'] ?>">
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <button type="submit" name="submit" data-bs-toggle="tooltip"
+                                                    data-bs-placement="top" title="Hapus"
+                                                    class="btn btn-link text-danger text-gradient px-1 mb-0"><i
+                                                        class="far fa-trash-alt me-2"></i></button>
+                                            </form>
+                                            <a href="/editSupply/<?= $supply['id_supply'] ?>" type="button"
+                                                class="btn btn-link text-dark px-1 mb-0"><i
+                                                    class="fa fa-pencil"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
                                 <?php
                                 endforeach
                                 ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Barang Keluar -->
+        <div class="col-6">
+            <div class="card mb-4">
+                <div class="card-header pb-0">
+                    <div class="row">
+                        <div class="col-6 d-flex align-items-center">
+                            <h6 class="mb-0">Barang Masuk</h6>
+                        </div>
+                        <div class="col-6 text-end">
+                            <a href="/tambahSupply" type="button" class="btn btn-outline-primary btn-sm mb-0">Tambah</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No
+                                    </th>
+                                    <th
+                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Jenis</th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Jumlah</th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Nominal</th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Tanggal</th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-bold"></span>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0"></p>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <p class="text-xs font-weight-bold mb-0"></p>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-bold">Rp</span>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-bold"></span>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex" align="center">
+                                            <form method="POST" action="/hapusSupply/">
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <button type="submit" name="submit" data-bs-toggle="tooltip"
+                                                    data-bs-placement="top" title="Hapus"
+                                                    class="btn btn-link text-danger text-gradient px-1 mb-0"><i
+                                                        class="far fa-trash-alt me-2"></i></button>
+                                            </form>
+                                            <a href="/editSupply/" type="button"
+                                                class="btn btn-link text-dark px-1 mb-0"><i
+                                                    class="fa fa-pencil"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
+
                             </tbody>
                         </table>
                     </div>
