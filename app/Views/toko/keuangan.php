94ba1f189bfd5fc45b8a4ddd428154b681a36011
karinadityas <karina.adityasramadhanti2041@students.unila.ac.id>
2023-12-06 20:28:42 +0700

    bikin round cuy

--- app/Views/toko/keuangan.php
+++ app/Views/toko/keuangan.php
@@ -67,46 +67,14 @@
                                         </td>
                                         <td>
                                             <div class="d-flex">
-                                                <a href="<?= base_url('/hapusPemasukan/' . $pemasukan['id_pemasukan']) ?>" class="btn btn-link text-danger text-gradient px-1 mb-0" onclick="return confirm('Apakah anda yakin?')"><i class="far fa-trash-alt me-2"></i></a>
-                                                <button type="button" class="btn btn-link text-dark px-1 mb-0" data-bs-toggle="modal" data-bs-target="#editPemasukan-<?= $pemasukan['id_pemasukan'] ?>">
-                                                    <i class="fa fa-pencil"></i>
-                                                </button>
+                                                <form method="POST" action="/hapusPemasukan/<?= $pemasukan['id_pemasukan'] ?>">
+                                                    <input type="hidden" name="_method" value="DELETE">
+                                                    <button type="submit" name="submit" data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus" class="btn btn-link text-danger text-gradient px-1 mb-0"><i class="far fa-trash-alt me-2"></i></button>
+                                                </form>
+                                                <a href="/editPemasukan/<?= $pemasukan['id_pemasukan'] ?>" type="button" class="btn btn-link text-dark px-1 mb-0"><i class="fa fa-pencil"></i></a>
                                             </div>
                                         </td>
                                     </tr>
-                                    <!-- Modal Edit Pemasukan -->
-                                    <div class="modal fade" id="editPemasukan-<?= $pemasukan['id_pemasukan'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h6 class="modal-title" id="exampleModalLabel">Edit Pemasukan</h6>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true" style="color: black;">&times;</span></button>
-                                                </div>
-                                                <form action="<?= base_url('/updatePemasukan/' . $pemasukan['id_pemasukan']) ?>" method="post">
-                                                    <?= csrf_field(); ?>
-                                                    <div class="modal-body">
-                                                        <div class="form-group">
-                                                            <label for="sumber">Sumber</label>
-                                                            <select name="sumber" id="sumber" class="form-control">
-                                                                <option value="Toko">Toko</option>
-                                                            </select>
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <label for="tanggal">Tanggal</label>
-                                                            <input type="date" class="form-control" name="tanggal" id="tanggal" value="<?= $pemasukan['tanggal'] ?>">
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <label for="jumlah">Nominal</label>
-                                                            <input type="text" class="form-control" name="jumlah" id="jumlah" value="<?= $pemasukan['jumlah'] ?>">
-                                                        </div>
-                                                        <div align="center">
-                                                            <button type="submit" class="btn bg-gradient-dark mb-0">SIMPAN PERUBAHAN</button>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
                                 <?php
                                 endforeach
                                 ?>
@@ -120,7 +88,6 @@
         <div class="col-6">
             <div class="card mb-4">
                 <div class="card-header pb-0">
-
                     <div class="row">
                         <div class="col-6 d-flex align-items-center">
                             <h6 class="mb-0">Pengeluaran</h6>
@@ -129,89 +96,59 @@
                             <a href="/tambahPengeluaran" type="button" class="btn btn-outline-primary btn-sm mb-0">Tambah</a>
                         </div>
                     </div>
+                </div>
 
-                    <div class="card-body px-0 pt-0 pb-2">
-                        <div class="table-responsive p-0">
-                            <table class="table align-items-center mb-0">
-                                <thead>
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No
+                                    </th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Sumber</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Tanggal</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Nominal</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Aksi</th>
+                                    <th class="text-secondary opacity-7"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $no = 1;
+                                foreach ($pengeluaran as $pengeluaran) :
+                                ?>
                                     <tr>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No
-                                        </th>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                            Keperluan</th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                            Tanggal</th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                            Nominal</th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                            Aksi</th>
-                                        <th class="text-secondary opacity-7"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php $no = 1;
-                                    foreach ($pengeluaran as $pengeluaran) :
-                                    ?>
-                                        <tr>
-                                            <td class="align-middle text-center">
-                                                <span class="text-secondary text-xs font-weight-bold"><?= $no++ ?></span>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0"><?= $pengeluaran['keperluan'] ?></p>
-                                            </td>
-                                            <td class="align-middle text-center text-sm">
-                                                <p class="text-xs font-weight-bold mb-0"><?= $pengeluaran['tanggal'] ?></p>
-                                            </td>
-                                            <td class="align-middle text-center">
-                                                <span class="text-secondary text-xs font-weight-bold">Rp <?= number_format($pengeluaran['nominal']) ?></span>
-                                            </td>
-                                            <td>
-                                                <div class="d-flex">
-                                                    <a href="<?= base_url('/hapusPengeluaran/' . $pengeluaran['id_pengeluaran']) ?>" class="btn btn-link text-danger text-gradient px-1 mb-0" onclick="return confirm('Apakah anda yakin?')"><i class="far fa-trash-alt me-2"></i></a>
-                                                    <button type="button" class="btn btn-link text-dark px-1 mb-0" data-bs-toggle="modal" data-bs-target="#editPengeluaran-<?= $pengeluaran['id_pengeluaran'] ?>">
-                                                        <i class="fa fa-pencil"></i>
-                                                    </button>
-                                                </div>
-                                            </td>
-                                        </tr>
-
-                                        <!-- Modal Edit Pengeluaran -->
-                                        <div class="modal fade" id="editPengeluaran-<?= $pengeluaran['id_pengeluaran'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h6 class="modal-title" id="exampleModalLabel">Edit Pengeluaran</h6>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true" style="color: black;">&times;</span></button>
-                                                    </div>
-                                                    <form action="<?= base_url('/updatePengeluaran/' . $pengeluaran['id_pengeluaran']) ?>" method="post">
-                                                        <?= csrf_field(); ?>
-                                                        <div class="modal-body">
-                                                            <div class="form-group">
-                                                                <label for="keperluan">Keperluan</label>
-                                                                <input type="text" class="form-control" name="keperluan" id="keperluan" value="<?= $pengeluaran['keperluan'] ?>">
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <label for="tanggal">Tanggal</label>
-                                                                <input type="date" class="form-control" name="tanggal" id="tanggal" value="<?= $pengeluaran['tanggal'] ?>">
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <label for="jumlah">Nominal</label>
-                                                                <input type="text" class="form-control" name="nominal" id="nominal" value="<?= $pengeluaran['nominal'] ?>">
-                                                            </div>
-                                                            <div align="center">
-                                                                <button type="submit" class="btn bg-gradient-dark mb-0">SIMPAN PERUBAHAN</button>
-                                                            </div>
-                                                        </div>
-                                                    </form>
-                                                </div>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-bold"><?= $no++ ?></span>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs font-weight-bold mb-0"><?= $pengeluaran['sumber'] ?></p>
+                                        </td>
+                                        <td class="align-middle text-center text-sm">
+                                            <p class="text-xs font-weight-bold mb-0"><?= $pengeluaran['tanggal'] ?></p>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-bold">Rp
+                                                <?= number_format($pengeluaran['jumlah']) ?></span>
+                                        </td>
+                                        <td>
+                                            <div class="d-flex">
+                                                <form method="POST" action="/hapusPengeluaran/<?= $pengeluaran['id_pengeluaran'] ?>">
+                                                    <input type="hidden" name="_method" value="DELETE">
+                                                    <button type="submit" name="submit" data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus" class="btn btn-link text-danger text-gradient px-1 mb-0"><i class="far fa-trash-alt me-2"></i></button>
+                                                </form>
+                                                <a href="/editPengeluaran/<?= $pengeluaran['id_pengeluaran'] ?>" type="button" class="btn btn-link text-dark px-1 mb-0"><i class="fa fa-pencil"></i></a>
                                             </div>
-                                        </div>
-                                    <?php
-                                    endforeach
-                                    ?>
-                                </tbody>
-                            </table>
-                        </div>
+                                        </td>
+                                    </tr>
+                                <?php
+                                endforeach
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
